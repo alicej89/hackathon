3b278483feeaaca5bbc36ed96fadc0b22c5357d7
alicej <alice.jodra@saint-gobain.com>
2023-12-15 14:29:52 +0100

    expo-camera 1st try

--- screens/CameraScreen.tsx
+++ screens/CameraScreen.tsx
@@ -1,19 +1,71 @@
-import { useNavigation } from "@react-navigation/native";
-import { Text, View } from "react-native";
-import { Button } from "react-native-paper";
+//import { useNavigation } from "@react-navigation/native";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+import { Button, ActivityIndicator } from "react-native-paper";
 
-interface CameraScreenProps {}
+import { Camera, CameraType } from 'expo-camera';
+import { useState } from "react";
 
-export function CameraScreen(props: CameraScreenProps) {
-  const navigation = useNavigation();
+export function CameraScreen() {
+  /* const navigation = useNavigation();
   function navigateToDetails() {
     navigation.navigate("Details" as never);
+  } */
+
+  const [type, setType] = useState(CameraType.back);
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+
+  if (!permission) {
+    return <ActivityIndicator animating={true} />
+  }
+
+  if (!permission.granted) {
+    return (
+      <View style={styles.container}>
+        <Text style={{ textAlign: 'center' }}>We need your permission to show the camera</Text>
+        <Button onPress={requestPermission}>Allow</Button>
+      </View>
+    );
+  }
+
+  function toggleCameraType() {
+    setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
   }
 
   return (
     <View>
-      <Text>CameraScreen</Text>
-      <Button onPress={navigateToDetails}>Go to Details</Button>
+      <Camera style={styles.camera} type={type}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={toggleCameraType}>
+            <Text style={styles.text}>Flip Camera</Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+});
